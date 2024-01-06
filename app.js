e911e3cdea801525877e31426ace718a6e519a79
Sabhi Sharma <2021is_sabhisharma_b@nie.ac.in>
2024-01-06 12:08:43 +0530

    cjs to esm

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const qr = require('qr-image');
-const fs = require('fs');
-const fs_extra = require('fs-extra');
-const path = require('path'); 
+import express from "express";
+import qr from "qr-image";
+import fs from "fs";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+const port = 3000;
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -14,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
 
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 
 });
 
@@ -23,21 +26,20 @@ app.post("/", function (req, res) {
     const url = req.body.URL;
 
     var qr_img = qr.image(url);
-    qr_img.pipe(fs.createWriteStream(path.join(__dirname, 'public/qr_img.png')));
+    // qr_img.pipe(fs.createWriteStream(path.join(__dirname, 'public/qr_img.png')));
+    qr_img.pipe(fs.createWriteStream(__dirname + "/public/qr_img.png"));
 
     res.render("index");
 
 });
 
-fs_extra.ensureDirSync(path.join(__dirname, 'public'));
-
 app.post("/result", function(req, res) {
     res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || port, () => {
     
     console.log("Server is running at port 3000.");
 })
 
-module.exports = app;
+export default app;
